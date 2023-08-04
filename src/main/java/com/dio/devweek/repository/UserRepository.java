709d8261890d05e056be1d580b879446949cfc4c
Amanda Tinoco <amanda_tinoco00@hotmail.com>
2023-08-04 15:56:29 -0300

    feat: add dev profile file

---
+++ src/main/java/com/dio/devweek/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.dio.devweek.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dio.devweek.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
